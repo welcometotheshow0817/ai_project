f539704b08efffc8d75634f74864d4669a968997
welcometotheshow0817 <taekyung100817@gmail.com>
2026-05-12 16:02:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
 st.title('나의 첫 웹서비스 만들기')
-st.text_input('현오는 턱돌이 입니다')
-st.selectbox('좋아하는 음식을 선택하세요!',['치킨','떡볶이', '마라탕'])
+a=st.text_input('현오는 턱돌이 입니다')
+b=st.selectbox('좋아하는 음식을 선택하세요!',['치킨','떡볶이', '마라탕'])
+if st.button('인사말 생성'):
+  st.write(a+'님, 안녕하세요!')
